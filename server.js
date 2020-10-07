703833c9aca1bddc051cfce2d9d00f12cf50b1f8
Cezary Los <cezary.los@neoteric.eu>
2020-10-07 23:29:43 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/jackpot-app'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/jackpot-app'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/jackpot-app/' }),
-);
+app.get('/*', function(req,res) {
 
+    res.sendFile(path.join(__dirname+'/dist/jackpot-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
